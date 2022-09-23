dfb9efdb742d50845021dbf39dfd67ff720695df
Steven Liou <stevenliou@gmail.com>
2022-09-23 11:52:35 -0400

    Can display video

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,11 +2,16 @@ import { Box, Stack } from '@mui/material';
 import { ChannelCard, VideoCard } from 'components';
 import React from 'react';
 
-function Videos({ videos }) {
-  console.log(videos);
+function Videos({ videos, direction }) {
+  if (!videos?.length) return 'Loading...';
   return (
     <div>
-      <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      <Stack
+        direction={direction || 'row'}
+        flexWrap="wrap"
+        justifyContent="start"
+        gap={2}
+      >
         {videos.map((item, idx) => (
           <Box key={idx}>
             {item.id.videoId && <VideoCard video={item} />}
