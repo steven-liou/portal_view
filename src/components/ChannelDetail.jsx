1a2e26d648a87dbf33db36f4c92f9927458bfb5e
Steven Liou <stevenliou@gmail.com>
2022-09-23 11:18:39 -0400

    Finishes channel detail page

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,10 +1,48 @@
-import React, { useState } from 'react';
+import { Box } from '@mui/system';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { ChannelCard, Videos } from '.';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 function ChannelDetail() {
+  const { id } = useParams();
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
+
+      setChannelDetail(data?.items[0]);
+
+      const videosData = await fetchFromAPI(
+        `search?channelId=${id}&part=snippet%2Cid&order=date`
+      );
+
+      setVideos(videosData?.items);
+    };
+
+    fetchResults();
+  }, [id]);
+
   return (
-    <div>
-      <p>Body</p>
-    </div>
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            height: '300px',
+            background:
+              'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+            zIndex: 10,
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop={'-110px'} />
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: '100px' } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
   );
 }
 
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,10 +3,10 @@ import { ChannelCard, VideoCard } from 'components';
 import React from 'react';
 
 function Videos({ videos }) {
-  console.log(videos)
+  console.log(videos);
   return (
     <div>
-      <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
+      <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
         {videos.map((item, idx) => (
           <Box key={idx}>
             {item.id.videoId && <VideoCard video={item} />}
